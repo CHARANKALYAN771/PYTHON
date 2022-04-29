aea3947ddef17a0ac2a5a5ede3f96f8360f18390
charan kalayan <102531183+CHARANKALYAN771@users.noreply.github.com>
2022-04-29 08:00:56 +0530

    Add files via upload

---
+++ medianOfArrays_int_float.py
@@ -0,0 +1,13 @@
+# print(ord("b"))
+# print(ord("A"))
+# x="100 98 90"
+# y=x.split(" ")
+# print(y)
+# z=[]
+# for i in range(len(y)):
+#     z.append(int(y[i]))
+# print(z)
+# for i in range(len(z)):
+#     print(chr(z[i]))
+x=y=0
+print(x,y)
